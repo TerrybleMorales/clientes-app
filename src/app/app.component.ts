ec60615619ce5dd2e7a272db4d57a79faae8a36e
TerrybleMorales <guillermoruiz3009@gmail.com>
2023-11-15 11:18:44 -0600

    Actualizacion de app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bienvenido a Angular';
+  title = 'Bienvenido a Angular.';
 
   curso: string = 'Curso Springt 5 con Angular 7';
   profesor: string = 'Andrés Guzmán';
